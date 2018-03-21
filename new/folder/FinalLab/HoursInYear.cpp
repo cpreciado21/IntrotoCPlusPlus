bba4b385f5371f239871c57822e94bbf74f8434f
Claudia <37095785+cpreciado21@users.noreply.github.com>
2018-03-21 16:25:44 -0400

    Add files via upload

--- new/folder/FinalLab/HoursInYear.cpp
+++ new/folder/FinalLab/HoursInYear.cpp
@@ -1,1 +1,20 @@
+//
+//
+//  Final Lab Examination
+//  Question 1
+//  Created by Claudia Preciado on 12/9/16.
+//
+//
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double hoursinyear = 0;
+    hoursinyear = (365 * 24);
+    cout << "There are " << hoursinyear << " hours in a non-leap year." << endl;
+    return 0;
+}
