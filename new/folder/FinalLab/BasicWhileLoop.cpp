9ba0c00493cadca14858bedc7c0b491e1da69f44
Claudia <37095785+cpreciado21@users.noreply.github.com>
2018-03-21 16:26:45 -0400

    Add files via upload

---
+++ new/folder/FinalLab/BasicWhileLoop.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  Q4
+//
+//  Created by Claudia Preciado on 12/11/16.
+//  Copyright © 2016 Claudia Preciado. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Converted into while loop" << endl;
+    int i = 3;
+    while (i <18)
+    {
+        cout << i*i << endl;
+        i = i +3;
+    }
+    return 0;
+}
